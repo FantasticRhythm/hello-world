6bb24017f1f29a6e8a370314347a24e135af8eba
FantasticRhythm <1145190104@qq.com>
2018-12-12 19:52:55 +0800

    helloworld add commit

--- Prime.c
+++ Prime.c
@@ -1,22 +1,45 @@
-/*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.求[100,9999]之内:(1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
-//2018/11/14 更新
-int sum_bit(int num) {
-  return 0;
+#include<stdio.h>
+int A(int x)
+{
+	if(x==0) return 1;
+	int b=0;
+	int i=2;
+	for(i=2;i<x;i++)
+	{
+		if((x%i)==0)
+		{b=1;break;}
+	}
+	return b;
 }
-
-int multi_bit(int num) {
-  return 0;
-}
-
-int square_sum_bit(int num) {
-  return 0;
-}
-
-bool isprime(int num) {
-  return false;
-}
-
-int main() {
-  if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113)))
-    //to do sth
+int main()
+{
+	int N=0,P=0,Max=0,i;
+	for(i=100;i<10000;i++)
+	{
+		int n=i,a[4],o=0;
+		while(n>0)
+		{
+			a[o]=n%10;
+			n=n/10;
+			o++;
+		}
+		int pl=0,mul=1,mp=0,j;
+		for(j=0;j<o;j++)
+		{
+			pl=pl+a[j];
+			mul=mul*a[j];
+			mp=mp+a[j]*a[j];
+		}
+		int q = A(pl) + A(mul) + A(mp) + A(i);
+		if(q==0)
+		{
+			printf("%d\n",i);
+			N++;
+			P=P+i;
+			if(i>Max)
+			Max=i;
+		}
+	}
+	printf("%d\n%d\n%d",N,P,Max);
+	return 0; 
 }
